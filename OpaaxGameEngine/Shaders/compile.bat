2d3bba519b3f03c1ec6a8b6d15061e631b4c5f82
Enguerran_COBERT <enguerran.cobert@gmail.com>
2025-04-16 16:21:11 -0400

    Remove all previous work (keepit local)
    Start engine with dll setup.

--- OpaaxGameEngine/Shaders/compile.bat
+++
@@ -1,3 +0,0 @@
-C:/Dev/Vulkan/Bin/glslc.exe SimpleShader.vert -o SimpleShader.vert.spv
-C:/Dev/Vulkan/Bin/glslc.exe SimpleShader.frag -o SimpleShader.frag.spv
-pause
---
+++ OpaaxGameEngine/Source/Opaax/OpaaxApplication.cpp
@@ -0,0 +1,16 @@
+#include "Opaax/OpaaxApplication.h"
+
+using namespace OPAAX;
+
+OpaaxApplication::OpaaxApplication()
+{
+}
+
+OpaaxApplication::~OpaaxApplication()
+{
+}
+
+void OPAAX::OpaaxApplication::Run()
+{
+	while (true);
+}
